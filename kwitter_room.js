a4e9d624e72217cae75727ba1b3a9361460abf1b
Volcano-Coder <99091122+Volcano-Coder@users.noreply.github.com>
2022-06-23 11:24:15 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+      apiKey: "AIzaSyDwGs9CLCqgPbB4VlQ168EvU6ubvoZzINw",
+      authDomain: "let-s-chat-d65c0.firebaseapp.com",
+      projectId: "let-s-chat-d65c0",
+      storageBucket: "let-s-chat-d65c0.appspot.com",
+      messagingSenderId: "1079461607265",
+      appId: "1:1079461607265:web:9ef58389cf3895965db720"
+    };
+    
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
